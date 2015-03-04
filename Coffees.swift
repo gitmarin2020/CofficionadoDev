1798918e00f404f770815179201b636bc058efb6
Raphael Pedrini <raphaelpedrini@gmail.com>
2015-03-04 02:50:59 -0300

    first run

---
+++ Coffees.swift
@@ -0,0 +1,21 @@
+//
+//  Coffees.swift
+//  CofficionadoTabbed
+//
+//  Created by Matt Oguz on 2/27/15.
+//  Copyright (c) 2015 VentureScience. All rights reserved.
+//
+
+import UIKit
+
+class Coffees: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CofficionadoTabbed/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CofficionadoTabbed
+//
+//  Created by Matt Oguz on 2/27/15.
+//  Copyright (c) 2015 VentureScience. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
